2187cbadbaf9cb85a425c846e2d516cd8e25b9bf
RoyKorthout <61917583+RoyKorthout@users.noreply.github.com>
2021-04-29 15:41:49 +0200

    Basic Template (with navbar)

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
 function SearchBar() {
-    return (null);
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul className="navbar-nav ml-auto mx-3">
+                    <li className="nav-item">
+                        <a style={{color: "white"}} className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a style={{color: "white"}} className="nav-link" href="#">Visualize</a>
+                    </li>
+                    <li className="nav-item">
+                        <a style={{color: "white"}} className="nav-link" href="#">Help</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+    );
 }
 
 export default SearchBar
--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,12 @@ import './custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
