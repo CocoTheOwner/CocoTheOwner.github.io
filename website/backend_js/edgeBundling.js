fceeb5eba5499de42e1b48548b98672042d805f6
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-17 13:49:56 +0200

    Update compiled .js files. Resolve errors with vis page

--- website/backend_js/edgeBundling.js
+++ website/backend_js/edgeBundling.js
@@ -1,108 +1,107 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mailGraph_1 = require("./mailGraph");
-var clusters_1 = require("./clusters");
-var csvData_js_1 = require("./csvData.js");
-// function sleep(ms: number) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-var EdgeBundling = /** @class */ (function () {
-    function EdgeBundling() {
-    }
-    EdgeBundling.prototype.drawArcs = function (element) {
-        var mailArray = csvData_js_1.emails;
-        // readCsv("../../enron-v1.csv", mailArray, {})
-        // await sleep(1000)
-        var graph = new mailGraph_1.MailGraph(mailArray);
-        var cl = new clusters_1.default();
-        var clustering = cl.stoC(graph, 1);
-        var links = cl.clusterLinks(graph, clustering);
-        var clusters = clustering.map(function (i) { return i.length; });
-        /*const clusters : number[] = [4, 4, 4, 20];
-        const links = [
-          [1, 4, 69],
-          [-1, 6, 20],
-          [-1, -1, 10],
-          [-1, -1, -1]
-        ]*/
-        var angles = [clusters.length];
-        // Make sure canvas is reachable
-        var canvas = document.getElementById("edgeBundling");
-        if (canvas == null) {
-            console.log("Failed to find canvas for visualisation with the name 'visCanv'");
-            return;
+define(["require", "exports", "./mailGraph", "./clusters", "./csvData"], function (require, exports, mailGraph_1, clusters_1, csvData_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    // function sleep(ms: number) {
+    //   return new Promise(resolve => setTimeout(resolve, ms));
+    // }
+    var EdgeBundling = /** @class */ (function () {
+        function EdgeBundling() {
         }
-        var ctx = canvas.getContext("2d");
-        if (ctx == null) {
-            console.log("Failed to get context from canvas.");
-            return;
-        }
-        // Set line width
-        ctx.lineWidth = 10;
-        // Retrieve amount of elements that were clustered
-        var elements = 0;
-        clusters.forEach(function (c) { return elements += c; });
-        console.log("Circle has " + elements + " elements across " + clusters.length + " indices");
-        // Angle and color counter
-        var angle = 0;
-        var colnr = 0;
-        // Get position on screen + radius
-        var x = canvas.width / 2;
-        var y = canvas.height / 2;
-        var r = canvas.height / 3;
-        // Loop over arc parts and show them
-        for (var index = 0; index < clusters.length; index++) {
-            // Retrieve current element
-            var element_1 = clusters[index];
-            // Calculate new arc angle
-            var newang = angle + element_1 / elements * Math.PI * 2;
-            // Draw arc
-            ctx.beginPath();
-            angles[colnr] = (angle + newang) / 2;
-            ctx.strokeStyle = selectColor(colnr++, clusters.length);
-            ctx.arc(x, y, r, angle, newang);
-            ctx.arc(x, y, r, newang, angle, true); // Required to prevent weird loop behaviour
-            ctx.closePath();
-            ctx.stroke();
-            // Log information
-            console.log("Drawing (" + x + "/" + y + ") w" + r + " @ " + angle / Math.PI + " to " + newang / Math.PI);
-            // Update angle
-            angle = newang;
-        }
-        var lineWidthMod = 200;
-        for (var index = 0; index < links.length; index++) {
-            var row = links[index];
-            var p1Angle = angles[index];
-            var p1x = x + r * Math.cos(p1Angle);
-            var p1y = y + r * Math.sin(p1Angle);
-            for (var jndex = 0; jndex < row.length; jndex++) {
-                var element_2 = row[jndex];
-                if (element_2 === 0 || element_2 === -1) {
-                    continue;
-                }
-                var p2Angle = angles[jndex + 1];
-                var p2x = x + r * Math.cos(p2Angle);
-                var p2y = y + r * Math.sin(p2Angle);
-                //https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Circle-trig6.svg/250px-Circle-trig6.svg.png
-                ctx.lineWidth = element_2 / lineWidthMod;
-                ctx.strokeStyle = selectColor(angles.length, clusters.length);
+        EdgeBundling.prototype.drawArcs = function (element) {
+            var mailArray = csvData_1.emails;
+            // readCsv("../../enron-v1.csv", mailArray, {})
+            // await sleep(1000)
+            var graph = new mailGraph_1.MailGraph(mailArray);
+            var cl = new clusters_1.default();
+            var clustering = cl.stoC(graph, 1);
+            var links = cl.clusterLinks(graph, clustering);
+            var clusters = clustering.map(function (i) { return i.length; });
+            /*const clusters : number[] = [4, 4, 4, 20];
+            const links = [
+              [1, 4, 69],
+              [-1, 6, 20],
+              [-1, -1, 10],
+              [-1, -1, -1]
+            ]*/
+            var angles = [clusters.length];
+            // Make sure canvas is reachable
+            var canvas = document.getElementById("edgeBundling");
+            if (canvas == null) {
+                console.log("Failed to find canvas for visualisation with the name 'visCanv'");
+                return;
+            }
+            var ctx = canvas.getContext("2d");
+            if (ctx == null) {
+                console.log("Failed to get context from canvas.");
+                return;
+            }
+            // Set line width
+            ctx.lineWidth = 10;
+            // Retrieve amount of elements that were clustered
+            var elements = 0;
+            clusters.forEach(function (c) { return elements += c; });
+            console.log("Circle has " + elements + " elements across " + clusters.length + " indices");
+            // Angle and color counter
+            var angle = 0;
+            var colnr = 0;
+            // Get position on screen + radius
+            var x = canvas.width / 2;
+            var y = canvas.height / 2;
+            var r = canvas.height / 3;
+            // Loop over arc parts and show them
+            for (var index = 0; index < clusters.length; index++) {
+                // Retrieve current element
+                var element_1 = clusters[index];
+                // Calculate new arc angle
+                var newang = angle + element_1 / elements * Math.PI * 2;
+                // Draw arc
                 ctx.beginPath();
-                ctx.moveTo(p1x, p1y);
-                ctx.lineTo(p2x, p2y);
+                angles[colnr] = (angle + newang) / 2;
+                ctx.strokeStyle = selectColor(colnr++, clusters.length);
+                ctx.arc(x, y, r, angle, newang);
+                ctx.arc(x, y, r, newang, angle, true); // Required to prevent weird loop behaviour
                 ctx.closePath();
                 ctx.stroke();
+                // Log information
+                console.log("Drawing (" + x + "/" + y + ") w" + r + " @ " + angle / Math.PI + " to " + newang / Math.PI);
+                // Update angle
+                angle = newang;
             }
-        }
-        /*
-          We now have an array of angles, where they are in order and represent an angle from 0* (right) to the middle of its arc
-          We have a list of links that need to connect these angle positions with lines, representing a connection
-        */
-    };
-    return EdgeBundling;
-}());
-function selectColor(colorNum, colors) {
-    if (colors < 1)
-        colors = 1; // defaults to one color - avoid divide by zero
-    return "hsl(" + (colorNum * (360 / colors) % 360) + ",100%,50%)";
-}
-new EdgeBundling().drawArcs(document.getElementById("edgeBundling"));
+            var lineWidthMod = 200;
+            for (var index = 0; index < links.length; index++) {
+                var row = links[index];
+                var p1Angle = angles[index];
+                var p1x = x + r * Math.cos(p1Angle);
+                var p1y = y + r * Math.sin(p1Angle);
+                for (var jndex = 0; jndex < row.length; jndex++) {
+                    var element_2 = row[jndex];
+                    if (element_2 === 0 || element_2 === -1) {
+                        continue;
+                    }
+                    var p2Angle = angles[jndex + 1];
+                    var p2x = x + r * Math.cos(p2Angle);
+                    var p2y = y + r * Math.sin(p2Angle);
+                    //https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Circle-trig6.svg/250px-Circle-trig6.svg.png
+                    ctx.lineWidth = element_2 / lineWidthMod;
+                    ctx.strokeStyle = selectColor(angles.length, clusters.length);
+                    ctx.beginPath();
+                    ctx.moveTo(p1x, p1y);
+                    ctx.lineTo(p2x, p2y);
+                    ctx.closePath();
+                    ctx.stroke();
+                }
+            }
+            /*
+              We now have an array of angles, where they are in order and represent an angle from 0* (right) to the middle of its arc
+              We have a list of links that need to connect these angle positions with lines, representing a connection
+            */
+        };
+        return EdgeBundling;
+    }());
+    function selectColor(colorNum, colors) {
+        if (colors < 1)
+            colors = 1; // defaults to one color - avoid divide by zero
+        return "hsl(" + (colorNum * (360 / colors) % 360) + ",100%,50%)";
+    }
+    new EdgeBundling().drawArcs(document.getElementById("edgeBundling"));
+});
--- website/backend_js/mailGraph.js
+++ website/backend_js/mailGraph.js
@@ -1,128 +1,130 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MailGraph = void 0;
-/** A class representing a directed graph for email traffic. */
-var MailGraph = /** @class */ (function () {
-    /**
-     * Create a new graph based on an interval between two dates.
-     * If any one of the indices is not given, we assume the beginning/end of time.
-     *
-     * Please remember that the `to` time is _exclusive_. E-mails from this day are **not** added.
-    */
-    function MailGraph(mailArr, from, to) {
-        if (from === void 0) { from = new Date(0); }
-        if (to === void 0) { to = new Date(1); }
-        this.graph = {};
-        this.emails = mailArr;
-        var startIndex = findTimeIndex(mailArr, from);
-        var endIndex = findTimeIndex(mailArr, to);
-        // Loop through all emails in the interval
-        while (startIndex < endIndex) {
-            var mail = this.emails[startIndex++];
-            // If this is fromId's first mail, add fromId to the graph as a node.
-            if (!(mail.fromId in this.graph)) {
-                this.graph[mail.fromId] = {};
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.MailGraph = void 0;
+    /** A class representing a directed graph for email traffic. */
+    var MailGraph = /** @class */ (function () {
+        /**
+         * Create a new graph based on an interval between two dates.
+         * If any one of the indices is not given, we assume the beginning/end of time.
+         *
+         * Please remember that the `to` time is _exclusive_. E-mails from this day are **not** added.
+        */
+        function MailGraph(mailArr, from, to) {
+            if (from === void 0) { from = new Date(0); }
+            if (to === void 0) { to = new Date(1); }
+            this.graph = {};
+            this.emails = mailArr;
+            var startIndex = findTimeIndex(mailArr, from);
+            var endIndex = findTimeIndex(mailArr, to);
+            // Loop through all emails in the interval
+            while (startIndex < endIndex) {
+                var mail = this.emails[startIndex++];
+                // If this is fromId's first mail, add fromId to the graph as a node.
+                if (!(mail.fromId in this.graph)) {
+                    this.graph[mail.fromId] = {};
+                }
+                // If this mail is the first fromId->toId mail, add toId to fromId's neighbours
+                if (!(mail.toId in this.graph[mail.fromId])) {
+                    this.graph[mail.fromId][mail.toId] = [];
+                }
+                this.graph[mail.fromId][mail.toId].push(mail.appreciation);
             }
-            // If this mail is the first fromId->toId mail, add toId to fromId's neighbours
-            if (!(mail.toId in this.graph[mail.fromId])) {
-                this.graph[mail.fromId][mail.toId] = [];
-            }
+        }
+        // Add a new edge (email) to the graph.
+        // If any one of the required nodes is missing, they are added.
+        MailGraph.prototype.addMail = function (mail) {
             this.graph[mail.fromId][mail.toId].push(mail.appreciation);
-        }
-    }
-    // Add a new edge (email) to the graph.
-    // If any one of the required nodes is missing, they are added.
-    MailGraph.prototype.addMail = function (mail) {
-        this.graph[mail.fromId][mail.toId].push(mail.appreciation);
-    };
-    MailGraph.prototype.copynodes = function () {
-        var nodes = [];
-        for (var key in this.graph) {
-            nodes.push(parseInt(key));
-        }
-        return nodes;
-    };
-    MailGraph.prototype.neighbours = function (node) {
-        var neighbours = [];
-        for (var key in this.graph[node]) {
-            neighbours.push(parseInt(key));
-        }
-        return neighbours;
-    };
+        };
+        MailGraph.prototype.copynodes = function () {
+            var nodes = [];
+            for (var key in this.graph) {
+                nodes.push(parseInt(key));
+            }
+            return nodes;
+        };
+        MailGraph.prototype.neighbours = function (node) {
+            var neighbours = [];
+            for (var key in this.graph[node]) {
+                neighbours.push(parseInt(key));
+            }
+            return neighbours;
+        };
+        /**
+         * Distance function
+         * Input: ID's of two employees, A and B
+         * Output: #emails A->B + #emails B->A
+        */
+        MailGraph.prototype.distance = function (employeeA, employeeB) {
+            var count = 0;
+            // Number of emails from A to B
+            try { //dirty check to prevent non existant mails
+                count += this.graph[employeeA][employeeB].length;
+            }
+            catch (_a) { }
+            // Number of emails from B to A
+            try {
+                count += this.graph[employeeB][employeeA].length;
+            }
+            catch (_b) { }
+            return count;
+        };
+        //distance of 2 clusters (for edgebundling)
+        MailGraph.prototype.clusterDist = function (c1, c2) {
+            var dist = 0;
+            for (var _i = 0, c1_1 = c1; _i < c1_1.length; _i++) {
+                var i = c1_1[_i];
+                for (var _a = 0, c2_1 = c2; _a < c2_1.length; _a++) {
+                    var j = c2_1[_a];
+                    dist += this.distance(i, j);
+                }
+            }
+            return dist;
+        };
+        return MailGraph;
+    }());
+    exports.MailGraph = MailGraph;
     /**
-     * Distance function
-     * Input: ID's of two employees, A and B
-     * Output: #emails A->B + #emails B->A
+     * Finds the index of the first or last mail sent on the specified date, based on whether we look on the low end or the high end.
+     * If no such date exists, the index of the date right after what we looked for is returned.
+     * lowEnd does not have to be specified. If it isn't, default value `true` will be used.
+     *
+     * Two special cases are included for returning the first and last index of the array:
+     * ```let i1 = findTimeIndex(new Date(0));  // Returns 0```
+     * ```let i2 = findTimeIndex(new Date(1));  // Returns emails.length```
     */
-    MailGraph.prototype.distance = function (employeeA, employeeB) {
-        var count = 0;
-        // Number of emails from A to B
-        try { //dirty check to prevent non existant mails
-            count += this.graph[employeeA][employeeB].length;
+    function findTimeIndex(mailArr, date) {
+        if (date.getTime() === 0) { // Special case: Return the first element of the array
+            return 0;
         }
-        catch (_a) { }
-        // Number of emails from B to A
-        try {
-            count += this.graph[employeeB][employeeA].length;
+        else if (date.getTime() === 1) { // Special case: Return the last element of the array
+            return mailArr.length;
         }
-        catch (_b) { }
-        return count;
-    };
-    //distance of 2 clusters (for edgebundling)
-    MailGraph.prototype.clusterDist = function (c1, c2) {
-        var dist = 0;
-        for (var _i = 0, c1_1 = c1; _i < c1_1.length; _i++) {
-            var i = c1_1[_i];
-            for (var _a = 0, c2_1 = c2; _a < c2_1.length; _a++) {
-                var j = c2_1[_a];
-                dist += this.distance(i, j);
+        else { // Normal case: Perform binary search to find the correct indices.
+            var l = 0;
+            var r = mailArr.length - 1;
+            // While true, keep searching
+            while (l <= r) {
+                var m = Math.floor((l + r) / 2);
+                if (mailArr[m].date < date) {
+                    l = m + 1;
+                }
+                else if (mailArr[m].date > date) {
+                    r = m - 1;
+                }
+                else {
+                    // If we found the right date, look for duplicates that may
+                    //   occur before or after the current item.
+                    while (mailArr[--m].date.getTime() === date.getTime()) { }
+                    ;
+                    // Return the index of the first or last occurrance of our date.
+                    // First if we look for a date on the low end, last if not.
+                    return m + 1;
+                }
             }
+            // If we did not find our date in the array, return the next acceptable thing.
+            // Return the first date after what we looked for.
+            return r + 1;
         }
-        return dist;
-    };
-    return MailGraph;
-}());
-exports.MailGraph = MailGraph;
-/**
- * Finds the index of the first or last mail sent on the specified date, based on whether we look on the low end or the high end.
- * If no such date exists, the index of the date right after what we looked for is returned.
- * lowEnd does not have to be specified. If it isn't, default value `true` will be used.
- *
- * Two special cases are included for returning the first and last index of the array:
- * ```let i1 = findTimeIndex(new Date(0));  // Returns 0```
- * ```let i2 = findTimeIndex(new Date(1));  // Returns emails.length```
-*/
-function findTimeIndex(mailArr, date) {
-    if (date.getTime() === 0) { // Special case: Return the first element of the array
-        return 0;
     }
-    else if (date.getTime() === 1) { // Special case: Return the last element of the array
-        return mailArr.length;
-    }
-    else { // Normal case: Perform binary search to find the correct indices.
-        var l = 0;
-        var r = mailArr.length - 1;
-        // While true, keep searching
-        while (l <= r) {
-            var m = Math.floor((l + r) / 2);
-            if (mailArr[m].date < date) {
-                l = m + 1;
-            }
-            else if (mailArr[m].date > date) {
-                r = m - 1;
-            }
-            else {
-                // If we found the right date, look for duplicates that may
-                //   occur before or after the current item.
-                while (mailArr[--m].date.getTime() === date.getTime()) { }
-                ;
-                // Return the index of the first or last occurrance of our date.
-                // First if we look for a date on the low end, last if not.
-                return m + 1;
-            }
-        }
-        // If we did not find our date in the array, return the next acceptable thing.
-        // Return the first date after what we looked for.
-        return r + 1;
-    }
-}
+});
