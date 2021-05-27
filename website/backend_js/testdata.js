fbbf4c38a984321b02d4b7ae78225fd3c71305f1
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-27 17:17:27 +0200

    remove testdata file

--- website/backend_js/testdata.js
+++
@@ -1,8 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    let apple = [
-        1, 2, 3, 4, 50, 6, 7, 8, 9
-    ];
-    exports.default = apple;
-});
--- website/typescript/testdata.ts
+++
@@ -1,4 +0,0 @@
-let apple: Number[] = [
-    1, 2, 3, 4, 50, 6, 7, 8, 9
-]
-export default apple
