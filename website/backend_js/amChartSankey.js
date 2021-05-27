5dbde10c3d73b131dfe81c382b9f0f51234e553d
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-27 17:17:53 +0200

    Fix some lacking compiled js files

--- website/backend_js/amChartSankey.js
+++ website/backend_js/amChartSankey.js
@@ -1,42 +1,44 @@
-define(["require", "exports", "./testdata"], function (require, exports, testdata_1) {
+define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    exports.chart = void 0;
     var am4core = window["am4core"];
     var am4charts = window["am4charts"];
     var am4themes_animated = window["am4themes_animated"];
+    const apple = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     /* Chart code */
     // Themes begin
     am4core.useTheme(am4themes_animated);
     // Themes end
-    var chart = am4core.create("sankeydiv", am4charts.SankeyDiagram);
-    chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
-    chart.data = [
-        { from: "A", to: "D", value: testdata_1.default[0] },
-        { from: "B", to: "D", value: testdata_1.default[1] },
-        { from: "B", to: "E", value: testdata_1.default[2] },
-        { from: "C", to: "E", value: testdata_1.default[3] },
-        { from: "D", to: "G", value: testdata_1.default[4] },
-        { from: "D", to: "I", value: testdata_1.default[5] },
-        { from: "D", to: "H", value: testdata_1.default[6] },
-        { from: "E", to: "H", value: testdata_1.default[7] },
-        { from: "G", to: "J", value: testdata_1.default[8] },
-        { from: "I", to: "J", value: testdata_1.default[9] }
+    exports.chart = am4core.create("sankeydiv", am4charts.SankeyDiagram);
+    exports.chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+    exports.chart.data = [
+        { from: "A", to: "D", value: apple[0] },
+        { from: "B", to: "D", value: apple[1] },
+        { from: "B", to: "E", value: apple[2] },
+        { from: "C", to: "E", value: apple[3] },
+        { from: "D", to: "G", value: apple[4] },
+        { from: "D", to: "I", value: apple[5] },
+        { from: "D", to: "H", value: apple[6] },
+        { from: "E", to: "H", value: apple[7] },
+        { from: "G", to: "J", value: apple[8] },
+        { from: "I", to: "J", value: apple[9] }
     ];
-    var hoverState = chart.links.template.states.create("hover");
+    var hoverState = exports.chart.links.template.states.create("hover");
     hoverState.properties.fillOpacity = 0.6;
-    chart.dataFields.fromName = "from";
-    chart.dataFields.toName = "to";
-    chart.dataFields.value = "value";
+    exports.chart.dataFields.fromName = "from";
+    exports.chart.dataFields.toName = "to";
+    exports.chart.dataFields.value = "value";
     // for right-most label to fit
-    chart.paddingRight = 30;
+    exports.chart.paddingRight = 30;
     // make nodes draggable
-    var nodeTemplate = chart.nodes.template;
+    var nodeTemplate = exports.chart.nodes.template;
     nodeTemplate.inert = true;
     nodeTemplate.readerTitle = "Drag me!";
     nodeTemplate.showSystemTooltip = true;
     nodeTemplate.width = 20;
     // make nodes draggable
-    var nodeTemplate = chart.nodes.template;
+    var nodeTemplate = exports.chart.nodes.template;
     nodeTemplate.readerTitle = "Click to show/hide or drag to rearrange";
     nodeTemplate.showSystemTooltip = true;
     nodeTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer;
--- website/backend_js/fileInput.js
+++ website/backend_js/fileInput.js
@@ -1,16 +1,30 @@
-define(["require", "exports", "./csvParser", "./csvData"], function (require, exports, csvParser_1, csvData_1) {
+define(["require", "exports", "./csvParser", "./csvData", "./amChartSankey"], function (require, exports, csvParser_1, csvData_1, amChartSankey_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    const lookup = {}; // create lookuptable
+    var emails = Object.assign([], csvData_1.emails);
+    var lookup = Object.assign([], csvData_1.lookup);
     const input = document.querySelector('input[type="file"]'); //get file input
     //when input changes, update file
     input.addEventListener('change', function (e) {
         const reader = new FileReader();
         reader.onload = function () {
-            csvParser_1.readCsv(reader.result, csvData_1.emails, lookup);
+            csvParser_1.readCsv(reader.result, emails, lookup);
         };
         reader.readAsText(input.files[0]);
         document.getElementById("f_input").value = null; //reset input
-        console.log(csvData_1.emails.length);
+        console.log("File uploaded successfully");
+        amChartSankey_1.chart.data = [
+            { from: "A", to: "D", value: 20 },
+            { from: "B", to: "D", value: 20 },
+            { from: "B", to: "E", value: 20 },
+            { from: "C", to: "E", value: 20 },
+            { from: "D", to: "G", value: 20 },
+            { from: "D", to: "I", value: 20 },
+            { from: "D", to: "H", value: 20 },
+            { from: "E", to: "H", value: 20 },
+            { from: "G", to: "J", value: 20 },
+            { from: "I", to: "J", value: 20 }
+        ];
+        amChartSankey_1.chart.validateData();
     });
 });
