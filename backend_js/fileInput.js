bbed2a765c6209530d39e5fdb21d7f88f5ba6a4a
Ruxi <ruxi.hristache@yahoo.com>
2021-05-31 12:53:36 +0200

    pop-up still crusty
    the user cannot visualize anything unless a data set is uploaded

--- backend_js/fileInput.js
+++ backend_js/fileInput.js
@@ -13,7 +13,10 @@ define(["require", "exports", "./csvParser", "./csvData", "./visualisationContro
         reader.readAsText(input.files[0]);
         document.getElementById("f_input").value = null; //reset input
         console.log("File uploaded successfully");
+
+        // after the file was uploaded, close the pop-up
         modal.style.display = "none";
+        
         visualisationController_1.updateCharts(emails, lookup, 8);
     });
 });
@@ -22,28 +25,8 @@ define(["require", "exports", "./csvParser", "./csvData", "./visualisationContro
  * Pop-up window for upload button 
  */
 
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
+// when the visualize page is ran, ask the user to upload the data set
 window.onload = function() {
   modal.style.display = "block";
 }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
