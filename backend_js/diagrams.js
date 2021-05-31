38f24a5f616dc0954a7c0b332cea6875d5fc8212
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-05-31 17:11:50 +0200

    Add job to diagrams so it's loaded

--- backend_js/diagrams.js
+++ backend_js/diagrams.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "./amChartChord", "./amChartSankey", "./fileInput"], function (require, exports) {
+define(["require", "exports", "./amChartChord", "./amChartChordJob", "./amChartSankey", "./fileInput"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
 });
