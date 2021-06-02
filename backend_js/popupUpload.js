2b6266f9d6ce4adaf4f9fafa4009dcf5325aa391
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-06-02 09:58:38 +0200

    Remove forgotten console logs

--- backend_js/popupUpload.js
+++ backend_js/popupUpload.js
@@ -1,10 +1,8 @@
 /**
  * Pop-up window for upload button 
  */
-console.log("Popup!")
 var modal = document.getElementById("myModal");
 // when the visualize page is ran, ask the user to upload the data set
 window.onload = function() {
-    console.log("Popup2!")
     modal.style.display = "block";
 }
