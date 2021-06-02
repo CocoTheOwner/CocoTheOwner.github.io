0fb816e39df231a7d00dcf2b670d503104a6b66b
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-06-02 10:34:06 +0200

    Fix 2nd input button. Let file name be visible.

--- backend_js/fileInput.js
+++ backend_js/fileInput.js
@@ -3,18 +3,28 @@ define(["require", "exports", "./csvParser", "./csvData", "./visualisationContro
     Object.defineProperty(exports, "__esModule", { value: true });
     var emails = Object.assign([], csvData_1.emails);
     var lookup = Object.assign([], csvData_1.lookup);
-    const input = document.querySelector('input[type="file"]'); //get file input
+    const input1 = document.getElementById("f_input_popup"); //get file input
     //when input changes, update file
-    input.addEventListener('change', function (e) {
+    input1.addEventListener('change', function (e) {
         const reader = new FileReader();
         reader.onload = function () {
             csvParser_1.readCsv(reader.result, emails, lookup);
         };
-        reader.readAsText(input.files[0]);
-        document.getElementById("f_input").value = null; //reset input
+        reader.readAsText(input1.files[0]);
         console.log("File uploaded successfully");
         var modal = document.getElementById("myModal");
         modal.style.display = "none";
+        visualisationController_1.updateCharts(emails, lookup, 8);
+    });
+    const input2 = document.getElementById("f_input"); //get file input
+    //when input changes, update file
+    input2.addEventListener('change', function (e) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            csvParser_1.readCsv(reader.result, emails, lookup);
+        };
+        reader.readAsText(input2.files[0]);
+        console.log("File uploaded successfully");
         visualisationController_1.updateCharts(emails, lookup, 8);
     });
 });
--- typescript/fileInput.ts
+++ typescript/fileInput.ts
@@ -4,18 +4,30 @@ import {updateCharts} from "./visualisationController"
 
 var emails = Object.assign([], e);
 var lookup = Object.assign([], l);
-const input = document.querySelector('input[type="file"]'); //get file input
+const input1 = document.getElementById("f_input_popup"); //get file input
 
 //when input changes, update file
-input.addEventListener('change', function (e) {
+input1.addEventListener('change', function (e) {
     const reader = new FileReader();
     reader.onload = function () {
         readCsv(reader.result as string, emails, lookup);
     };
-    reader.readAsText((input as HTMLInputElement).files[0]);
-    (document.getElementById("f_input") as HTMLInputElement).value = null; //reset input
+    reader.readAsText((input1 as HTMLInputElement).files[0]);
     console.log("File uploaded successfully");
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
+    updateCharts(emails, lookup, 8);
+});
+
+const input2 = document.getElementById("f_input"); //get file input
+
+//when input changes, update file
+input2.addEventListener('change', function (e) {
+    const reader = new FileReader();
+    reader.onload = function () {
+        readCsv(reader.result as string, emails, lookup);
+    };
+    reader.readAsText((input2 as HTMLInputElement).files[0]);
+    console.log("File uploaded successfully");
     updateCharts(emails, lookup, 8);
 });
