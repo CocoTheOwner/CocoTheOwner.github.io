8d6c8ffd1d04e778b8df4f84e6f5f86d8fca8048
The1Rogue <fabiantschut@gmail.com>
2021-06-03 16:35:09 +0200

    color fix + optimizations

--- backend_js/visualisationController.js
+++ backend_js/visualisationController.js
@@ -7,7 +7,7 @@ define(["require", "exports", "./amChartChord", "./amChartSankey", "./mailGraph"
         updateSankey(emails, lookup, sankeyClusters);
         updateChord(emails, lookup);
         amChartSankey_1.default.validateData(); // Updates the sankeyChart
-        amChartChord_1.default.validateData(); // Updates the chord diagram 
+        amChartChord_1.default.validateData(); // Updates the chord diagram
     }
     exports.updateCharts = updateCharts;
     function updateSankey(emails, lookup, clusters = 8) {
@@ -17,13 +17,13 @@ define(["require", "exports", "./amChartChord", "./amChartSankey", "./mailGraph"
         const timeframe = emails[emails.length - 1].date.getTime() - emails[0].date.getTime();
         // Calculate the dates between which the clusters exist.
         const dates = []; // Will contain as first element the first date and as the last, the last.
-        for (let i = 0; i <= clusters; i++) {
-            dates[i] = emails[0].date.getTime() + timeframe / clusters * i;
+        for (let i = 0; i < clusters; i++) {
+            dates[i] = emails[0].date.getTime() + timeframe / clusters * (i + 1);
         }
         // Loop over all mails and set counters, totals and colors
         let jobInfo = {};
-        let mailCounters = {};
-        let timeslot = 1;
+        let mailCounters = { 0: { "total": 1 } };
+        let timeslot = 0;
         for (let mailNum in emails) {
             // Get the mail
             let mail = emails[mailNum];
@@ -32,116 +32,60 @@ define(["require", "exports", "./amChartChord", "./amChartSankey", "./mailGraph"
             let tjob = lookup[mail.toId].jobTitle;
             // Check if the f/tjobs are in the jobID list, and add them if not
             if (jobInfo[fjob] === undefined) {
-                jobInfo[fjob] = {
-                    id: Object.keys(jobInfo).length,
-                    color: amChartSankey_1.default.colors.next(),
-                    name: fjob
-                };
+                jobInfo[fjob] = amChartSankey_1.default.colors.next();
             }
             if (jobInfo[tjob] === undefined) {
-                jobInfo[tjob] = {
-                    id: Object.keys(jobInfo).length,
-                    color: amChartSankey_1.default.colors.next(),
-                    name: tjob
-                };
+                jobInfo[tjob] = amChartSankey_1.default.colors.next();
             }
             // Get the date (as a numeric value)
             let date = mail.date.getTime();
             // Check if we are in the next timeslot
-            if (date > dates[timeslot])
-                timeslot += 1;
-            // Make sure there is a section for the timeslot and the fromJob
-            if (mailCounters[timeslot] === undefined)
-                mailCounters[timeslot] = {};
+            if (date > dates[timeslot]) {
+                timeslot++;
+                mailCounters[timeslot] = { "total": 0 };
+            }
+            // Add one to the counter of the current timeslot
+            mailCounters[timeslot]["total"]++;
+            // Make sure there is a section for the fromJob
             if (mailCounters[timeslot][fjob] === undefined)
                 mailCounters[timeslot][fjob] = {};
-            // Add one to the counter of the current timeslot
-            if (mailCounters[timeslot]["total"] === undefined)
-                mailCounters[timeslot]["total"] = 1;
-            else
-                mailCounters[timeslot]["total"] += 1;
             // Add one to the counter of the current fromJob->toJob
             if (mailCounters[timeslot][fjob][tjob] === undefined)
                 mailCounters[timeslot][fjob][tjob] = 1;
             else
-                mailCounters[timeslot][fjob][tjob] += 1;
+                mailCounters[timeslot][fjob][tjob]++;
         }
+        // Reset sankey data
+        amChartSankey_1.default.data = [];
         // Calculate fractions per job for the timeslot it is in
         for (let timeslot in mailCounters) {
             // Get total for timeslot
             let timeslotTotal = mailCounters[timeslot]["total"];
             // Loop over all jobs and get their total (in the timeslot) and calculate&save fraction
-            for (let j in jobInfo) {
-                // Skip undefined
-                if (jobInfo[j] === undefined)
-                    continue;
-                let fjob = jobInfo[j].name;
+            for (let fjob in jobInfo) {
+                if (timeslot == "0") {
+                    amChartSankey_1.default.data.push(addSankeyColor(fjob, jobInfo[fjob]));
+                }
                 // Make sure the dict exists
                 if (mailCounters[timeslot][fjob] === undefined)
                     mailCounters[timeslot][fjob] = {};
                 // Loop over the to jobs
-                for (let k in jobInfo) {
-                    // Skip undefined
-                    if (jobInfo[k] === undefined)
-                        continue;
-                    let tjob = jobInfo[k].name;
-                    // Make sure the dict exists
-                    if (mailCounters[timeslot][fjob][tjob] === undefined) {
-                        mailCounters[timeslot][fjob][tjob] = 0;
-                        continue;
-                    }
-                    // Retrieve the job total (this is from fjob and to tjob)
-                    let jobTotal = mailCounters[timeslot][fjob][tjob];
+                for (let tjob in jobInfo) {
+                    // Retrieve the job total if it exists, else 0
+                    let jobTotal = mailCounters[timeslot][fjob][tjob] ? mailCounters[timeslot][fjob][tjob] : 0;
                     // Calculate the fraction for the timeslot
                     let fraction = jobTotal / timeslotTotal * 100;
-                    // Store the fraction instead of the counter (overwritten)
-                    mailCounters[timeslot][fjob][tjob] = fraction;
+                    // add the number to sankey
+                    amChartSankey_1.default.data.push(addSankeyConnection(fjob, tjob, Number(timeslot), fraction, jobInfo[tjob]));
                 }
             }
         }
-        // Reset sankey data
-        amChartSankey_1.default.data = [];
-        for (let i in mailCounters) {
-            // Turn the timeslot string into a number, and count from 0
-            let timeslot = Number(i) - 1;
-            // Loop over the from jobs
-            for (let j in jobInfo) {
-                let fjob = jobInfo[j];
-                // Loop over the to jobs
-                for (let k in jobInfo) {
-                    let tjob = jobInfo[k];
-                    // Get the fraction value for this datapoint
-                    let value = 0;
-                    try {
-                        value = mailCounters[String(timeslot + 1)][fjob.name][tjob.name];
-                    }
-                    catch (_a) {
-                        console.log(fjob + " / " + tjob + " " + timeslot);
-                        continue;
-                    }
-                    // Add a datapoint to the chart
-                    amChartSankey_1.default.data.push(addSankeyConnection(fjob.name, tjob.name, timeslot, value, jobInfo));
-                }
-                // If there are no connections, the color is not set. Hence this line.
-                amChartSankey_1.default.data.push(addSankeyColor(fjob.name, timeslot, jobInfo));
-            }
-        }
     }
-    function addSankeyColor(fjob, timeslot, jobInfo) {
-        // Make the "from" string, which is either, (CEO as example):
-        // 0 CEO (0)     -> First entry
-        // 0 (X)         -> Xth timeslot entry
-        let from = undefined;
-        if (timeslot === 0) {
-            from = fjob + ".0";
-        }
-        else {
-            from = fjob.slice(0, 3).toUpperCase().replace(" ", "") + "." + timeslot;
-        }
-        let color = jobInfo[fjob].color;
-        return { from: from, color: color };
+    //make a sankey data entry to give the first layer a color
+    function addSankeyColor(fjob, color) {
+        return { from: fjob + ".0", color: color };
     }
-    function addSankeyConnection(fjob, tjob, timeslot, value, jobInfo) {
+    function addSankeyConnection(fjob, tjob, timeslot, value, color) {
         // Make the "from" string, which is either, (CEO as example):
         // 0 CEO (0)     -> First entry
         // 0 (X)         -> Xth timeslot entry
@@ -154,8 +98,6 @@ define(["require", "exports", "./amChartChord", "./amChartSankey", "./mailGraph"
         }
         // Make the "to" string, which is similar to the second entry for "from", but one higher
         let to = tjob.slice(0, 3).toUpperCase().replace(" ", "") + "." + (timeslot + 1);
-        // Color
-        let color = jobInfo[tjob].color;
         // Return entry to the diagram
         return { from: from, to: to, value: value, color: color };
     }
--- typescript/visualisationController.ts
+++ typescript/visualisationController.ts
@@ -11,7 +11,7 @@ export function updateCharts(emails: Email[], lookup: {[id: number]: Employee}, 
     updateChord(emails, lookup)
 
     sankeyChart.validateData(); // Updates the sankeyChart
-    chordChart.validateData(); // Updates the chord diagram 
+    chordChart.validateData(); // Updates the chord diagram
 }
 
 function updateSankey(emails: Email[], lookup: {[id: number]: Employee}, clusters = 8): void {
@@ -24,14 +24,14 @@ function updateSankey(emails: Email[], lookup: {[id: number]: Employee}, cluster
 
     // Calculate the dates between which the clusters exist.
     const dates = [] // Will contain as first element the first date and as the last, the last.
-    for (let i = 0; i <= clusters; i++) {
-        dates[i] = emails[0].date.getTime() + timeframe/clusters*i
+    for (let i = 0; i < clusters; i++) {
+        dates[i] = emails[0].date.getTime() + timeframe / clusters * (i+1)
     }
 
     // Loop over all mails and set counters, totals and colors
     let jobInfo = {}
-    let mailCounters = {}
-    let timeslot = 1
+    let mailCounters = {0: {"total":1}}
+    let timeslot = 0
     for (let mailNum in emails){
 
         // Get the mail
@@ -43,133 +43,73 @@ function updateSankey(emails: Email[], lookup: {[id: number]: Employee}, cluster
 
         // Check if the f/tjobs are in the jobID list, and add them if not
         if (jobInfo[fjob] === undefined) {
-            jobInfo[fjob] = {
-                id: Object.keys(jobInfo).length,
-                color: sankeyChart.colors.next(),
-                name: fjob
-            }
+            jobInfo[fjob] = sankeyChart.colors.next()
         }
         if (jobInfo[tjob] === undefined) {
-            jobInfo[tjob] = {
-                id: Object.keys(jobInfo).length,
-                color: sankeyChart.colors.next(),
-                name: tjob
-            }
+            jobInfo[tjob] = sankeyChart.colors.next()
         }
 
         // Get the date (as a numeric value)
         let date = mail.date.getTime()
 
         // Check if we are in the next timeslot
-        if (date > dates[timeslot]) timeslot += 1
-
-        // Make sure there is a section for the timeslot and the fromJob
-        if (mailCounters[timeslot] === undefined) mailCounters[timeslot] = {}
-        if (mailCounters[timeslot][fjob] === undefined) mailCounters[timeslot][fjob] = {}
+        if (date > dates[timeslot]){
+            timeslot++
+            mailCounters[timeslot] = {"total": 0}
+        }
 
         // Add one to the counter of the current timeslot
-        if (mailCounters[timeslot]["total"] === undefined) mailCounters[timeslot]["total"] = 1
-        else mailCounters[timeslot]["total"] += 1
+        mailCounters[timeslot]["total"]++
+
+        // Make sure there is a section for the fromJob
+        if (mailCounters[timeslot][fjob] === undefined) mailCounters[timeslot][fjob] = {}
 
         // Add one to the counter of the current fromJob->toJob
         if (mailCounters[timeslot][fjob][tjob] === undefined) mailCounters[timeslot][fjob][tjob] = 1
-        else mailCounters[timeslot][fjob][tjob] += 1
-    }
-
-    // Calculate fractions per job for the timeslot it is in
-    for (let timeslot in mailCounters){
-        
-        // Get total for timeslot
-        let timeslotTotal = mailCounters[timeslot]["total"]
-
-        // Loop over all jobs and get their total (in the timeslot) and calculate&save fraction
-        for (let j in jobInfo){
-
-            // Skip undefined
-            if (jobInfo[j] === undefined) continue
-            let fjob = jobInfo[j].name
-
-            // Make sure the dict exists
-            if (mailCounters[timeslot][fjob] === undefined) mailCounters[timeslot][fjob] = {}
-
-            // Loop over the to jobs
-            for (let k in jobInfo){
-                
-                // Skip undefined
-                if (jobInfo[k] === undefined) continue
-                let tjob = jobInfo[k].name
-
-                // Make sure the dict exists
-                if (mailCounters[timeslot][fjob][tjob] === undefined) {
-                    mailCounters[timeslot][fjob][tjob] = 0
-                    continue
-                }
-
-                // Retrieve the job total (this is from fjob and to tjob)
-                let jobTotal = mailCounters[timeslot][fjob][tjob]
-
-                // Calculate the fraction for the timeslot
-                let fraction = jobTotal / timeslotTotal * 100
-
-                // Store the fraction instead of the counter (overwritten)
-                mailCounters[timeslot][fjob][tjob] = fraction
-            }
-        }
+        else mailCounters[timeslot][fjob][tjob]++
     }
 
     // Reset sankey data
     sankeyChart.data = []
 
-    for (let i in mailCounters){
+    // Calculate fractions per job for the timeslot it is in
+    for (let timeslot in mailCounters){
 
-        // Turn the timeslot string into a number, and count from 0
-        let timeslot = Number(i) - 1
+        // Get total for timeslot
+        let timeslotTotal = mailCounters[timeslot]["total"]
 
-        // Loop over the from jobs
-        for (let j in jobInfo){
-            let fjob = jobInfo[j]
+        // Loop over all jobs and get their total (in the timeslot) and calculate&save fraction
+        for (let fjob in jobInfo){
 
-            // Loop over the to jobs
-            for (let k in jobInfo){
-                let tjob = jobInfo[k]
-
-                // Get the fraction value for this datapoint
-                let value = 0
-                try {
-                    value = mailCounters[String(timeslot+1)][fjob.name][tjob.name]
-                } catch {
-                    console.log(fjob + " / " + tjob + " " + timeslot)
-                    continue
-                }
-
-                // Add a datapoint to the chart
-                sankeyChart.data.push(addSankeyConnection(fjob.name, tjob.name, timeslot, value, jobInfo))
+            if (timeslot == "0"){
+                 sankeyChart.data.push(addSankeyColor(fjob, jobInfo[fjob]))
             }
 
-            // If there are no connections, the color is not set. Hence this line.
-            sankeyChart.data.push(addSankeyColor(fjob.name, timeslot, jobInfo))
+
+            // Make sure the dict exists
+            if (mailCounters[timeslot][fjob] === undefined) mailCounters[timeslot][fjob] = {}
+
+            // Loop over the to jobs
+            for (let tjob in jobInfo){
+
+                // Retrieve the job total if it exists, else 0
+                let jobTotal = mailCounters[timeslot][fjob][tjob] ? mailCounters[timeslot][fjob][tjob] : 0
+
+                // Calculate the fraction for the timeslot
+                let fraction = jobTotal / timeslotTotal * 100
+
+                // add the number to sankey
+                sankeyChart.data.push(addSankeyConnection(fjob, tjob, Number(timeslot), fraction, jobInfo[tjob]))
+            }
         }
     }
 }
-function addSankeyColor(fjob: string, timeslot: number, jobInfo: {}): {from: string, color}{
-
-    // Make the "from" string, which is either, (CEO as example):
-    // 0 CEO (0)     -> First entry
-    // 0 (X)         -> Xth timeslot entry
-    let from = undefined
-    if (timeslot === 0) { 
-        from = fjob + ".0"
-    }
-    else {
-        from = fjob.slice(0, 3).toUpperCase().replace(" ", "") + "." + timeslot
-    }
-
-    let color = jobInfo[fjob].color
-
-    return {from: from, color: color}
+//make a sankey data entry to give the first layer a color
+function addSankeyColor(fjob: string, color: string): {from: string, color}{
+    return {from: fjob + ".0", color: color}
 }
 
-function addSankeyConnection(fjob: string, tjob: string, timeslot: number, value: number, jobInfo: {}): {from: string, to: string, value: number, color} {
+function addSankeyConnection(fjob: string, tjob: string, timeslot: number, value: number, color: string): {from: string, to: string, value: number, color} {
     // Make the "from" string, which is either, (CEO as example):
     // 0 CEO (0)     -> First entry
     // 0 (X)         -> Xth timeslot entry
@@ -183,9 +123,6 @@ function addSankeyConnection(fjob: string, tjob: string, timeslot: number, value
     
     // Make the "to" string, which is similar to the second entry for "from", but one higher
     let to = tjob.slice(0, 3).toUpperCase().replace(" ", "") + "." + (timeslot + 1)
-
-    // Color
-    let color = jobInfo[tjob].color
 
 
     // Return entry to the diagram
