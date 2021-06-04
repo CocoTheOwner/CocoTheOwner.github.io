ba6b4b2207488b79d79960160935a9e8110a5b82
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-06-04 16:45:59 +0200

    Give sankeybar round edges. Patch position.

--- typescript/sankeyBar.ts
+++ typescript/sankeyBar.ts
@@ -46,6 +46,7 @@ const canvas = <HTMLCanvasElement> document.getElementById("sankeyCanvas");
 
 // Set the internal pixel width for the bar
 canvas.width = 1000
+canvas.height = 15
 
 // Magic number that patches an unexplainable rescaling bug
 var magic_number = 0.833
@@ -62,11 +63,11 @@ function drawSankeyBar(){
 
     // Fill the canvas with a light pink block (the background)
     ctx.fillStyle = "rgb(169, 169, 255)"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    roundRect(ctx, 0, 0, canvas.width, canvas.height, 7.5, true, false)
 
     // Fill the selection with a dark purple block (the foreground)
     ctx.fillStyle = "#AB00CC"
-    ctx.fillRect(canvas.width * fraction[0] * magic_number, 0, canvas.width * fraction[1] * magic_number, canvas.height)
+    roundRect(ctx, canvas.width * fraction[0] * magic_number, 0, canvas.width * fraction[1] * magic_number, canvas.height, 7.5, true, false)
 }
 
 export default function updateData(dates: string[], fractions: number[]) {
@@ -75,3 +76,57 @@ export default function updateData(dates: string[], fractions: number[]) {
     drawSankeyLabels()
     drawSankeyBar()
 }
+
+/**
+ * Full credits for the function go to Juan Mendes, who gave this as an answer on StackOverflow
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} [radius = 5] The corner radius; It can also be an object 
+ *                 to specify different radii for corners
+ * @param {Number} [radius.tl = 0] Top left
+ * @param {Number} [radius.tr = 0] Top right
+ * @param {Number} [radius.br = 0] Bottom right
+ * @param {Number} [radius.bl = 0] Bottom left
+ * @param {Boolean} [fill = false] Whether to fill the rectangle.
+ * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
+ */
+ function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+    if (typeof stroke === 'undefined') {
+      stroke = true;
+    }
+    if (typeof radius === 'undefined') {
+      radius = 5;
+    }
+    if (typeof radius === 'number') {
+      radius = {tl: radius, tr: radius, br: radius, bl: radius};
+    } else {
+      var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+      for (var side in defaultRadius) {
+        radius[side] = radius[side] || defaultRadius[side];
+      }
+    }
+    ctx.beginPath();
+    ctx.moveTo(x + radius.tl, y);
+    ctx.lineTo(x + width - radius.tr, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    ctx.lineTo(x + width, y + height - radius.br);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    ctx.lineTo(x + radius.bl, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    ctx.lineTo(x, y + radius.tl);
+    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    ctx.closePath();
+    if (fill) {
+      ctx.fill();
+    }
+    if (stroke) {
+      ctx.stroke();
+    }
+  
+  }
