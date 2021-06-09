83557c8c6aafd569766958b7cb03ece01890abf1
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-06-09 12:32:15 +0200

    Remove junk, remove draggable

--- typescript/amChartSankey.ts
+++ typescript/amChartSankey.ts
@@ -40,8 +40,8 @@ chart.paddingRight = 150;
  * customize nodes
  */
 nodeTemplate.inert = true;
-nodeTemplate.readerTitle = "Drag me!";
-nodeTemplate.showSystemTooltip = true;
+nodeTemplate.draggable = false;
+nodeTemplate.showSystemTooltip = false;
 nodeTemplate.width = 10;
 nodeTemplate.height = 20;
 nodeTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer
@@ -62,18 +62,9 @@ linkTemplate.fillOpacity = 0.2;
 // clicking nodes
 let node;
 nodeTemplate.events.on("hit", function (event) {
-  if (node == event.target) {
-    node.fill = node.defaultState.properties.fill;
-    node = undefined;
-  }
-  else {
-    if (node) {
-      node.fill = node.defaultState.properties.fill;
-    }
-    node = event.target;   
-    console.log("node " + node.name + " was clicked");
-    node.setState("selected");
-  }
+  console.log("node " + event.target.name + " was clicked");
+  event.target.nameLabel.label.text = ""
+  event.target.scale = event.target.scale * 1.1
 });
 
 // when a node is selected, colour it in red
